62209d889187053b6cd14bad619891f4f9fe6a38
Evgenvg78 <128367253+Evgenvg78@users.noreply.github.com>
2024-10-09 00:39:45 +0800

    Update simple_function.py

--- simple_function.py
+++ simple_function.py
@@ -1,9 +1,9 @@
-import csv
+# import csv
 
-def simple_hello_with_csv():
+def simple_csv():
     print("Hello from GitHub with CSV!")
-    data = [["Name", "Age"], ["John", "23"], ["Anna", "34"]]
-    with open("output.csv", "w", newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
-    print("CSV file created")
+    # data = [["Name", "Age"], ["John", "23"], ["Anna", "34"]]
+    # with open("output.csv", "w", newline='') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerows(data)
+    # print("CSV file created")
