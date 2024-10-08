d0bc457177a87fdc3d1eb2770e46e4dd15112707
Evgenvg78 <128367253+Evgenvg78@users.noreply.github.com>
2024-10-09 00:54:55 +0800

    Update simple_function.py

--- simple_function.py
+++ simple_function.py
@@ -2,7 +2,9 @@ import csv
 
 def simple_csv():
     print("Hello from GitHub with CSV!")
+    print('sssuukkaa')
     data = [["Name", "Age"], ["John", "23"], ["Anna", "34"]]
+    print('sssssssss')
     with open("output.csv", "w", newline='') as file:
         writer = csv.writer(file)
         writer.writerows(data)
