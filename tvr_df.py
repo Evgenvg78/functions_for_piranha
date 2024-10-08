499a86d297702007129b092a5bd7812be2b345a0
Evgenvg78 <128367253+Evgenvg78@users.noreply.github.com>
2024-10-08 23:25:12 +0800

    tvr_df.py

--- tvr_df.py
+++ tvr_df.py
@@ -1,21 +1,24 @@
+import csv
+import pandas as pd
+
 def TVR_transform(TVR):
-  with open(TVR, encoding='utf8') as file:
-      separator = file.readline()[0]  # Получаем элемент, который разделяет названия колонок
-  with open(TVR, encoding='utf8') as file:
-      column_names = file.readline().strip().split(separator)[2:]  # Получаем названия колонок начиная с третьего элемента
-  data = []
-  with open(TVR, encoding='utf8') as file:
-      lines = file.readlines()[1:]  # Пропускаем первую строку с заголовками
-      reader = csv.reader(lines, delimiter=' ')
-      for row in reader:
-          if len(row) > 2:
-              row[2] = ' '.join(row[2:])  # Объединяем оставшиеся элементы в третий столбец
-              data.append(row[:3])  # Добавляем только первые три элемента в список
+    with open(TVR, encoding='utf8') as file:
+        separator = file.readline()[0]  # Получаем элемент, который разделяет названия колонок
+    with open(TVR, encoding='utf8') as file:
+        column_names = file.readline().strip().split(separator)[2:]  # Получаем названия колонок начиная с третьего элемента
+    data = []
+    with open(TVR, encoding='utf8') as file:
+        lines = file.readlines()[1:]  # Пропускаем первую строку с заголовками
+        reader = csv.reader(lines, delimiter=' ')
+        for row in reader:
+            if len(row) > 2:
+                row[2] = ' '.join(row[2:])  # Объединяем оставшиеся элементы в третий столбец
+                data.append(row[:3])  # Добавляем только первые три элемента в список
 
-  # Преобразуем список в DataFrame
-  df = pd.DataFrame(data, columns=['stroka', 'stolbec', 'data'])
+    # Преобразуем список в DataFrame
+    df = pd.DataFrame(data, columns=['stroka', 'stolbec', 'data'])
 
-  # Изменяем тип данных на целочисленные
-  df['stroka'] = df['stroka'].astype(int)
-  df['stolbec'] = df['stolbec'].astype(int)
-  return df
+    # Изменяем тип данных на целочисленные
+    df['stroka'] = df['stroka'].astype(int)
+    df['stolbec'] = df['stolbec'].astype(int)
+    return df
